978d7974a1fadd1f59130b258e346a3f94879c22
taku.saito <taku.saito@nijibox.co.jp>
2017-11-30 11:54:50 +0900

    Hello

---
+++ app/src/main/java/com/example/nijimac097/auto_build_test/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.nijimac097.auto_build_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
